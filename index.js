98872c58561c8f9ff6be3d0b81f067be815f6de1
Gulsah Genc <gulsahgenc.gg@gmail.com>
2020-09-02 14:31:34 +0300

    Made vh property responsive

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const instagram = document.querySelector('#instagram');
 const codepen = document.querySelector('#codepen');
 const github = document.querySelector('#github');
 
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 // makes the first project active, others hidden
 projects.forEach(project => {
     project.classList.contains('first') ? project.classList.add('active')
@@ -58,3 +71,4 @@ github.addEventListener("click", function () {
 codepen.addEventListener("click", function () {
     window.open("https://codepen.io/GulsahG/#");
 });
+

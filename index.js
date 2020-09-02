cf9ec231748cf907bec1eb8826cf321737e0987a
Gulsah Genc <gulsahgenc.gg@gmail.com>
2020-09-02 14:51:49 +0300

    Changed typing animation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const discord = document.querySelector('#discord');
 const instagram = document.querySelector('#instagram');
 const codepen = document.querySelector('#codepen');
 const github = document.querySelector('#github');
-
+const secondLine = document.querySelector('.secondLine');
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
@@ -26,6 +26,11 @@ projects.forEach(project => {
     project.classList.contains('first') ? project.classList.add('active')
         : project.classList.add('hidden');
 });
+
+// makes the secondline not hidden after 2 seconds
+window.setTimeout(() => {
+    secondLine.style.display = "block";
+}, 2000);
 
 // makes the next project active on button click
 nextButtons.forEach(button => {
